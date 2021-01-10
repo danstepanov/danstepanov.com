b0be57b4c62195c63e84533b9d15a99ce58d0a58
danstepanov <danhstepanov@gmail.com>
2021-01-10 00:33:17 -0800

    add more content to timeline and projects

--- components/Timeline.js
+++ components/Timeline.js
@@ -31,14 +31,54 @@ const FullTimeline = () => (
         <Divider />
         <Year>2019</Year>
         <ul>
-            <Step title="Launched Westside Story">
+            <Step title="Launched Westside Story 🤗">
                 Built a close-knit community of 50+ good humans that really helped me get through a personally challenging year.
             </Step>
         </ul>
+        <Divider />
         <Year>2018</Year>
         <ul>
-            <Step title="Moved to LA">
-                It was really cold in New York.
+            <Step title="Moved to LA 🏝️">
+            </Step>
+        </ul>
+        <Divider />
+        <Year>2017</Year>
+        <ul>
+            <Step title="Joined Slice Capital 🍊">
+                Recent regulations made it possible for regular people to invest in startups and I set out to make that possible. This was my first time building out an entire platform from scratch. I used React, Apollo, GraphQL, Serverless, and Postgres.
+            </Step>
+        </ul>
+        <Divider />
+        <Year>2016</Year>
+        <ul>
+            <Step title="Joined Make School (YC W12) 🕹️">
+                Taught high school students how to build games with Swift in order to prepare them for working at FAANG companies or starting their own companies. 
+            </Step>
+            <Step title="Traveled through Asia with Hacker Paradise 🌏">
+                Traveled with a group of about 40 through Korea, Japan, Thailand, and Indonesia. This was one of the most fun experiences of my life. 
+            </Step>
+        </ul>
+        <Divider />
+        <Year>2015</Year>
+        <ul>
+            <Step title="Graduated from Penn State 🎓">
+                Got my chemical engineering degree and my student debt, I was now a full-fledged adult. 
+            </Step>
+            <Step title="Moved to Palo Alto 🦄">
+            </Step>
+        </ul>
+        <Divider />
+        <Year>2008</Year>
+        <ul>
+            <Step title="Built my First Game 🎮">
+                I wasn't aware of it at the time but I used BASIC to build a Family Guy themed text-based adventure game on my TI-83 Plus calculator. Afterwards, a classmate deleted 30 lines of code and I stopped coding until college.  
+            </Step>
+        </ul>
+        <Divider />
+        <Year>2002</Year>
+        <ul>
+            <Step title="Built my First Computer 🖥️">
+                With my uncle's help, I scoured the shelves of spare parts being sold by pop-up vendors in massive parking lots. Used what I could afford to assemble my first tower with a whopping 32gb hard drive! 
             </Step>
         </ul>
         <Divider />
@@ -55,18 +95,25 @@ const Timeline = () => {
     return (
         <>
             <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black">Timeline</h3>
+            <Year>2021</Year>
+            <ul>
+                <Step title="Launched New Website 🎉">
+                    New website, who dis? This website is built using NextJS, TailwindCSS, and is deployed via Vercel. I'm looking forward to adding a lot more to it over the coming weeks!
+                </Step>
+            </ul>
+            <Divider />
             <Year>2020</Year>
             <ul>
-                <Step title="Joined Marble">
+                <Step title="Joined Marble (YC S19) 👨‍💻">
                     Led the complete overhaul and full rewrite of Marble's user-facing application, moving it from Objective-C to React-Native and Typescript.
                 </Step>
-                <Step title="Supply LA Hospitals with Over 20k Face Shields">
+                <Step title="Supply LA Hospitals with Over 20k Face Shields 😷">
                     Joined Crash Space in their efforts to centralize everyone in LA with a 3D printer to mass produce face shields during a nationwide shortage. The group now uses injection moldings which is way more effective.
                 </Step>
                 <Step title="Moved to Santa Monica 🌴">
                     Who doesn't love living right by the beach? Watching the sunset from Palisades Park is heavenly.
                 </Step>
-                <Step title="On Deck Fellowship">
+                <Step title="On Deck Fellowship 🙌">
                     Honored to join this incredibly supportive community of makers. I'm regularly inspired by the people I talk to within On Deck.
                 </Step>
             </ul>
--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,9 @@ export default function Home() {
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black">Oh hai, I'm Dan</h1>
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black pt-8">Projects</h3>
-        <ProjectCard title="Marble Kiosk" description="Cashier-free checkout at restaurants via elegant kiosks that allow thematic customization." href="https://marblekiosk.com" icon="marble" />
+        <ProjectCard title="Marble (YC S19)" description="Cashier-free checkout at restaurants via elegant kiosks that allow thematic customization." href="https://marblekiosk.com" icon="marble" />
         <ProjectCard title="CRASH Covid" description="A distributed network of people with 3D printers working to provide PPE to hospitals in the greater LA area." href="https://covid.crashspace.org/" icon="crashCovid" />
+        <ProjectCard title="Slice Capital (acquired)" description="SEC and FINRA-approved funding platform providing startup investment to the masses." href="https://slice.capital" icon="slice" />
         <Timeline />
       </div>
     </Container>
