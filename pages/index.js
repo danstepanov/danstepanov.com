47bdcba80d58780248d11b3707010c26f74f3e4e
danstepanov <danhstepanov@gmail.com>
2021-11-22 11:18:23 -0500

    fix wrapping

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,10 @@ export default function Home() {
     <Container>
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">Oh hai, I'm Dan</h1>
-        <div className="flex flex-row">
-          <p className="text-black dark:text-white">I'm exploring new projects, particularly in biotech and longevity.&nbsp;</p>
-          <a className="text-blue-600 dark:text-white" href="/connect">Let's chat.</a>
+        <div className="flex flex-row flex-wrap">
+          <p className="text-black dark:text-white">I'm exploring new projects, particularly in biotech and longevity&nbsp;</p>
         </div>
+        <a className="text-blue-600" href="/connect">Let's chat</a>
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black pt-8 dark:text-white">Projects</h3>
         <ProjectCard title="ProPhone (YC W21)" description="ProPhone makes it easier to grow your business and spend more time in the field not in the office." href="https://www.prophone.com/" icon="prophone" />
         <ProjectCard title="Marble (YC S19)" description="Cashier-free checkout at restaurants via elegant kiosks that allow thematic customization." href="https://vimeo.com/471041260" icon="marble" />
