91b0f11d7f89914a6e92d1502f6d0c705cd48f43
danstepanov <danhstepanov@gmail.com>
2021-11-22 10:32:28 -0500

    remove acquired from slice capital

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <ProjectCard title="ProPhone (YC W21)" description="ProPhone makes it easier to grow your business and spend more time in the field not in the office." href="https://www.prophone.com/" icon="prophone" />
         <ProjectCard title="Marble (YC S19)" description="Cashier-free checkout at restaurants via elegant kiosks that allow thematic customization." href="https://vimeo.com/471041260" icon="marble" />
         <ProjectCard title="CRASH Covid" description="A distributed network of people with 3D printers working to provide PPE to hospitals in the greater LA area." href="https://covid.crashspace.org/" icon="crashCovid" />
-        <ProjectCard title="Slice Capital (acquired)" description="SEC and FINRA-approved funding platform providing startup investment to the masses." href="https://pitchbook.com/profiles/company/121401-01#overview" icon="slice" />
+        <ProjectCard title="Slice Capital" description="SEC and FINRA-approved funding platform providing startup investment to the masses." href="https://pitchbook.com/profiles/company/121401-01#overview" icon="slice" />
         <Timeline />
       </div>
     </Container>
