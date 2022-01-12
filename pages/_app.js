3d0904e7a5f7cf9eb194c0f2dbab73c421093765
danstepanov <danhstepanov@gmail.com>
2022-01-12 00:28:05 -0800

    add fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,32 @@
 import 'tailwindcss/tailwind.css'
 import '@/styles/global.css'
+import { useEffect } from 'react'
+import * as Fathom from 'fathom-client'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('IJYKSATR', {
+      includedDomains: ['danstepanov.com'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <ThemeProvider attribute="class">
       <Head>
