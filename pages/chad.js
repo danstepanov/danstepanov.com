5b9f310680a0b489ee477393012d4e356be17dee
danstepanov <danhstepanov@gmail.com>
2022-08-26 01:14:26 -0700

    add chad page

---
+++ pages/chad.js
@@ -0,0 +1,14 @@
+import Container from '../components/Container'
+
+export default function Chad() {
+    return (
+        <Container>
+            <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+                <p className="text-black dark:text-white">Internet historian, when are you going to upload a new main channel video? It's been like 4 months...</p>
+                <br></br>
+                <p className="text-black dark:text-white">Sincerely,</p>
+                <p className="text-black dark:text-white">The community</p>
+            </div>
+        </Container>
+    )
+}
