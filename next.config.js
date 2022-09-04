a3950eab7a2d936e86cf0965e5438b8a148d468d
danstepanov <danhstepanov@gmail.com>
2022-09-03 22:28:55 -0400

    update resume path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+    async rewrites() {
+        return [
+            {
+              source: '/resume',
+              destination: '/resume.pdf',
+            },
+          ]
+    },
     async redirects() {
         return [
             {
