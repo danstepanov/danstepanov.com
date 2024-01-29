839bfb9bcfb77ba78b9cc6a55bb210eb747e0500
Dan Stepanov <dan@danstepanov.com>
2024-01-29 12:10:05 -0500

    add masterplan link

--- next.config.js
+++ next.config.js
@@ -18,6 +18,11 @@ module.exports = {
                 source: '/discord',
                 destination: 'https://discord.gg/XS9qS2mvTR',
                 permanent: true
+            },
+            {
+                source: '/masterplan',
+                destination: 'https://link.excalidraw.com/l/398AFcdY0wd/7YTJYwQift',
+                permanent: true
             }
         ]
     }
