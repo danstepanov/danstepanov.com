7e24a0ddf89ef250ebd8baa5c823597195e2cbc5
Dan Stepanov <dan@danstepanov.com>
2024-08-22 22:57:20 -0700

    fix on mobile

--- src/app/components/SpeakingSection.js
+++ src/app/components/SpeakingSection.js
@@ -3,35 +3,33 @@ import React from 'react';
 import ProjectCard from './ProjectCard';
 
 const SpeakingEvent = ({ id, title, description, href, icon, labelTitle }) => (
-<div className="mb-4">
-<ProjectCard
-key={id}
-title={title}
-description={description}
-href={href}
-icon={icon}
-labelTitle={labelTitle}
-/>
-</div>
-);
+	<div className="mb-4">
+		<ProjectCard
+			key={id}
+			title={title}
+			description={description}
+			href={href}
+			icon={icon}
+			labelTitle={labelTitle}
+		/>
+	</div>
+	);
 
 const SpeakingSection = () => {
   const talks = [
     {
 		id: 0,
-      	title: "Being a Good Platform Citizen",
+      	title: "Chain React 2024",
       	description: "I talk about the importance of building native feeling experiences.",
       	href: "https://www.youtube.com/watch?v=3UEQhdaWwus",
 		icon: 'chain-react',
-		labelTitle: "Chain React 2024"
     },
     {
 		id: 1,
-      	title: "RNR Episode 298",
+      	title: "React Native Radio",
       	description: "I chat building developer tooling with Mazen, from the Infinite Red team.",
       	href: "https://www.youtube.com/watch?v=zJ584RPF3EE",
 		icon: 'react-native',
-		labelTitle: "React Native Radio"
     }
   ];
 
